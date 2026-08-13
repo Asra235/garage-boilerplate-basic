d373bc22a19fecbcb6f6faa5c22083808f5e44c9
s3431331 <s3431331@student.rmit.edu.au>
2026-08-13 20:55:36 +1000

    fix: null images
    Updated type to add optional null value

--- frontend/public/team.ts
+++ frontend/public/team.ts
@@ -2,7 +2,7 @@ type TeamMember = {
   name: string;
   role: string;
   bio: string;
-  image: string;
+  image: string | null;
 }
 
 
@@ -23,7 +23,7 @@ export const team:TeamMember[] = [
     name: 'Jonathan Fazzari',
     role: 'Developer',
     bio: 'Develops funky ui stuff, is also the dummy with the long bio, i guess this is where i waffle on until it breaches the normal size of the box. uhh, my favourite colour is green? what else can i talk about, my favourite food is lasagna. I need to waffle even more. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed euismod eros, luctus auctor erat. Mauris leo metus, tristique sit amet finibus sed, auctor non magna. Aliquam fermentum ac ex vel pulvinar. Pellentesque tempor pellentesque nibh, a vestibulum metus. Vivamus sed nisl blandit, feugiat tortor nec, vestibulum magna. ',
-    image: '/images-hardcoded/placeholder.png',
+    image: null,
   },
   {
     name: 'Finn Gurry',
@@ -35,6 +35,7 @@ export const team:TeamMember[] = [
     name: 'Bohan Chen',
     role: 'Business Analyst',
     bio: 'Does other stuff like analysing business',
-    image: '/images-hardcoded/placeholder.png',
+    // image: '/images-hardcoded/placeholder.png',
+    image: null,
   },
 ] as const
